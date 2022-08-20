79276f7298bedef01230a0694ef9fc08f88bfd3a
Andre Amboni <amboni.andre@gmail.com>
2022-08-20 11:45:13 -0300

    Fixing conflicts

--- init.lua
+++ init.lua
@@ -1,3 +1,1 @@
-require "andreamboni.set"
-require "andreamboni.plugins"
-require "andreamboni.keymap"
+require("neovim")
---
+++ lua/neovim/remap.lua
@@ -0,0 +1,4 @@
+local nnoremap = require("neovim.keymap").nnoremap
+
+nnoremap("<leader>pv", "<cmd>Ex<CR>")
+
